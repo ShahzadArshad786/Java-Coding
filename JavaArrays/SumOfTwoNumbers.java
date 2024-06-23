abcde05a515fc5f794621ab0d2dd7d0df6fe6579
Your Name <you@example.com>
2024-06-23 06:56:23 +0500

    "First Commit"

---
+++ JavaArrays/SumOfTwoNumbers.java
@@ -0,0 +1,22 @@
+package JavaArrays;
+import java.util.*;
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+
+    int num1 , num2 , sum = 0 ; 
+
+    System.out.print("Enter First  Number : ");
+    num1 = sc.nextInt();
+
+    System.out.print("Enter Second Number : ");
+    num2 = sc.nextInt();
+
+    sum = num1 + num2 ; 
+
+    sc.close();
+    }
+    
+}
